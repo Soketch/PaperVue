0fa1633dd1b4987d69afc7f30375714f6975fde1
zhen huang <94835875+Soketch@users.noreply.github.com>
2023-10-30 22:59:31 +0800

    Update childHeader.vue

--- src/components/childTool/childHeader.vue
+++ src/components/childTool/childHeader.vue
@@ -6,22 +6,22 @@
                     <a :href="href" @click="navigate">爬虫</a>
                 </li>
             </router-link>
-            <router-link exact to="/WorkBench/PaperEdit" v-slot="{ href, navigate, isActive }">
+            <router-link exact to="/PaperEdit" v-slot="{ href, navigate, isActive }">
                 <li class="navbar-item" :class="{ active: isActive }">
                     <a :href="href" @click="navigate">编辑</a>
                 </li>
             </router-link>
-            <router-link to="/WorkBench/SubScribe" v-slot="{ href, navigate, isActive }">
+            <router-link to="/SubScribe" v-slot="{ href, navigate, isActive }">
                 <li class="navbar-item" :class="{ active: isActive }">
                     <a :href="href" @click="navigate">订阅</a>
                 </li>
             </router-link>
-            <router-link to="/WorkBench/ChatGroup" v-slot="{ href, navigate, isActive }">
+            <router-link to="/ChatGroup" v-slot="{ href, navigate, isActive }">
                 <li class="navbar-item" :class="{ active: isActive }">
                     <a :href="href" @click="navigate">聊天室</a>
                 </li>
             </router-link>
-            <router-link to="/WorkBench/User" v-slot="{ href, navigate, isActive }">
+            <router-link to="/User" v-slot="{ href, navigate, isActive }">
                 <li class="navbar-item" :class="{ active: isActive }">
                     <a :href="href" @click="navigate"><svg t="1698406990641" class="icon" viewBox="0 0 1024 1024"
                             version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="9031" width="32" height="32">
